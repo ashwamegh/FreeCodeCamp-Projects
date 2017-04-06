7e4ee5e63f54fc523b63206086480759c7e0ba4a
shashank7200 <shashank7200@gmail.com>
2017-04-06 19:06:30 +0530

    Finished footer section

--- Portfolio/resources/js/script.js
+++ Portfolio/resources/js/script.js
@@ -45,3 +45,38 @@ $(function () {
             }
         });
     });
+
+/******** Copyright Year   **********/
+let date = new Date().getFullYear();
+document.getElementById('currentyear').innerHTML = date;
+
+
+
+/***** Scroll Animations *******/
+
+$('.js--wp-1').waypoint(function(direction){ 
+       
+        $('.js--wp-1').addClass('animated fadeIn');
+        
+     },
+    {
+        offset:'50%;'
+            });
+    
+    $('.js--wp-2').waypoint(function(direction){ 
+       
+        $('.js--wp-2').addClass('animated pulse');
+        
+     },
+    {
+        offset:'50%;'
+            });
+    
+    $('.js--wp-4').waypoint(function(direction){ 
+       
+        $('.js--wp-4').addClass('animated pulse');
+        
+     },
+    {
+        offset:'50%;'
+            });
