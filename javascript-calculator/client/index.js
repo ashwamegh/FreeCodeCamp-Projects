58f1634619c6853ce858c7520ed29886317b4f0f
shashank7200 <shashank7200@gmail.com>
2017-08-12 22:02:54 +0530

    javascript-calculator initial commit

---
+++ javascript-calculator/client/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './style.css';
+
+import App from './components/App.jsx';
+
+
+ReactDOM.render(<App/>, document.getElementById('root'));
+
+console.log('Loaded');
+console.log("foffkfkf");
